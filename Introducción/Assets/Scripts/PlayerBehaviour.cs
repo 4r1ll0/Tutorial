fc6a0c9573a58763ba609a55ab71881c99c24a9c
4r1ll0 <ivan.diaz0306@gmail.com>
2024-09-27 08:53:09 +0100

    27/09/2024
    Puse sonidos a las monedas y una música de ambiente al juego.

--- Introducción/Assets/Scripts/PlayerBehaviour.cs
+++ Introducción/Assets/Scripts/PlayerBehaviour.cs
@@ -10,6 +10,8 @@ public class PlayerBehaviour : MonoBehaviour
     public float speedMovement = 5f;
     public int coin = 0;
     public TextMeshProUGUI coinsText;
+    public AudioClip coinSFX;
+    public AudioClip coinSFXDos;
 
 
     // Start is called before the first frame update
@@ -41,17 +43,23 @@ public class PlayerBehaviour : MonoBehaviour
         {
             coin = coin + 1;// es igual coins += 1
             Debug.Log("He chocado con una moneda" + coin);
+            AudioSource.PlayClipAtPoint(coinSFX, transform.position);
+
         }
         else if (other.CompareTag("CoinSpecial"))
         {
             coin = coin + 5; // es igual coins += 5
             Debug.Log("He chocado con una moneda especial" + coin);
+            AudioSource.PlayClipAtPoint(coinSFXDos, transform.position);
+
         }
         if (other.tag.Contains("Coin")) //la palabra "contains" se refiere a un trozo de texto
         {
-            coinsText.text = coin.ToString();  
+            coinsText.text = coin.ToString();
             other.gameObject.SetActive(false);
+            AudioSource.PlayClipAtPoint(coinSFX, transform.position);
+
         }
-        
+      
     }
 }
